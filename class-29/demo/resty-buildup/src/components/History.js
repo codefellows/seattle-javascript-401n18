77b4b49b464da04b1f965b07f6513984601477a1
John Cokos <highlander.44@gmail.com>
2021-05-22 14:37:44 -0700

    round 2 updates

--- class-29/demo/resty-buildup/src/components/History.js
+++ class-29/demo/resty-buildup/src/components/History.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 class History extends React.Component {
 
+  // TODO: Tell the "Home" component that you clicked on one
+  // Then ... the Home component needs to tell the Form component new values for the form
   render() {
     return (
       <aside>
